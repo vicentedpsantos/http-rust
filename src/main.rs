67d3af19aba70ba7213b2d034830da6f41acd46a
Vicente <vicentedpsantos@gmail.com>
2021-02-22 10:25:17 -0300

    Organize code into multiple modules

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,12 @@
+use http::Request;
+use http::Method;
+use server::Server;
+
+mod server;
+mod http;
+
 fn main() {
     let server = Server::new("127.0.0.1:8080".to_string());
 
     server.run();
 }
-
-struct Server {
-    addr: String,
-}
-
-impl Server {
-    fn new(addr: String) -> Self {
-    	Self { addr }
-    }
-
-    fn run(self) {
-    	println!("Listening on {}", self.addr);	
-    }
-}
-
-struct Request {
-    path: String,
-    query_string: Option<String>,
-    method: Method,
-}
-
-enum Method {
-    GET,
-    DELETE,
-    POST,
-    PUT,
-    HEAD,
-    CONNECT,
-    OPTIONS,
-    TRACE,
-    PATCH
-}
-
-/*
-* GET /user?id=10 HTTP/1.1\r\n
-* HEADERS \r\n
-* BODY
-*/
