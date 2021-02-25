1664f787714e10cff38a617ba2b649bceebd77bf
Vicente <vicentedpsantos@gmail.com>
2021-02-25 10:03:38 -0300

    Implement Response

--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,7 @@
-use std::net::TcpListener;
-use std::convert::TryFrom;
 use crate::http::Request;
+use std::convert::TryFrom;
 use std::io::Read;
+use std::net::TcpListener;
 
 pub struct Server {
     addr: String,
@@ -13,29 +13,30 @@ impl Server {
     }
 
     pub fn run(self) {
-        println!("Listening on {}", self.addr);	
+        println!("Listening on {}", self.addr);
 
         let listener = TcpListener::bind(&self.addr).unwrap();
 
-	loop {
-	    match listener.accept() {
-	    	Ok((mut stream, _addr)) => {
-		    let mut buffer = [0; 1024];
-		    match stream.read(&mut buffer) {
-		    	Ok(_) => {
-		    	    println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+        loop {
+            match listener.accept() {
+                Ok((mut stream, _addr)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Received a request: {}", String::from_utf8_lossy(&buffer));
 
-			    match Request::try_from(&buffer as &[u8]) {
-			    	Ok(request) => {
-				},
-				Err(e) => println!("Failed to parse a request: {}", e),
-			    }
-		    	}
-			Err(e) => println!("Failed to read from connection: {}", e),
-		    }
-		},
-		Err(e) => println!("Failed to establish a connection: {}", e),
-	    }
-	}
+                            match Request::try_from(&buffer as &[u8]) {
+                                Ok(request) => {
+                                    dbg!(request);
+                                }
+                                Err(e) => println!("Failed to parse a request: {}", e),
+                            }
+                        }
+                        Err(e) => println!("Failed to read from connection: {}", e),
+                    }
+                }
+                Err(e) => println!("Failed to establish a connection: {}", e),
+            }
+        }
     }
 }
